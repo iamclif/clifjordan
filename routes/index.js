2c57049aed92ab5c3324792b903b3eb10a4af7e4
Clif Jordan <iamclif@gmail.com>
2017-08-03 22:32:18 -0400

    add some content

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Clif Jordan | Front End Developer' });
 });
 
 module.exports = router;
